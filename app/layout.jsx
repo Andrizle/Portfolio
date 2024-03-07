1a3127e68a63c59fbae0566ddd3aa6b33f0363c8
Andrew <andrew.madrigal23@gmail.com>
2024-03-07 10:55:29 -0800

    added google analytics tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-N06MTL2GX1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-N06MTL2GX1');
+      </script>
       <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap"/>
 
@@ -28,8 +37,8 @@ export default function RootLayout({ children }) {
 
         />
           {children}
-        
-        
+
+
         </body>
     </html>
   )
